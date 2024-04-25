e4a88d6a2fa90f1f5c5eec57b6134049f20969a3
sidhu47 <sidhuemailid@gmail.com>
2024-04-25 02:48:03 -0400

    new change

--- src/services/QuizService.js
+++ src/services/QuizService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const QUIZ_SERVICE_API_URL = "https://54.241.138.202:8080";
+const QUIZ_SERVICE_API_URL = "http://13.57.253.224:8080";
 
 class QuizService {
     authenticate(user) {
